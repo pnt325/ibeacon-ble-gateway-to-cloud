96157c452bc4394890e049f8a16404b24674a8c0
Phat Nguyen <46193669+pnt325@users.noreply.github.com>
2022-05-16 15:37:46 +0700

    gateway updating

--- firmware-blegateway/main/app_config.h
+++ firmware-blegateway/main/app_config.h
@@ -16,6 +16,8 @@
 #define APP_TASK_BUTTON_LED_SLEEP_PERIOD    50      // ms
 #define APP_LED_BLINKING_PERIOD             250     // ms
 
+#define APP_GATEWAY_DEVICE_MAX              16
+
 #define APP_GET_UNKNOW_DEVICE_TOPIC         "internal_net/req/cl/%02x:%02x:%02x:%02x:%02x:%02x"
 
 #endif /*_APP_CONFIG_H_*/
--- firmware-blegateway/main/gateway.c
+++ firmware-blegateway/main/gateway.c
@@ -5,9 +5,10 @@
  *      Author: Phat.N
  */
 
+#include <string.h>
 #include "gateway.h"
-
-// TODO limit of data field to 12.
+#include "app_config.h"
+#include "mqtt.h"
 
 #define DATA_FIELD_NAME_LEN  10
 // #define DATA_FIELD_VALUE_LEN 10
@@ -19,6 +20,7 @@
 #define DataTypeUInt_Str   "uint"
 #define DataTypeBool_str   "bool"
 
+/* Type =================================================== */
 typedef enum {
     TypeFloat,
     TypeString,
@@ -33,16 +35,89 @@ typedef struct  {
     data_type_t type;
 } data_field_t;
 
-typedef struct {    
-    char uuid[16];
-    data_field_t fields[DATA_FILED_MAX];
-}
+typedef struct  {
+    uint8_t addr[6];    // Device MAC address
+    uint8_t data[4];    // Beacon data
+    uint8_t prop;       // Properties
+} device_data_t;
 
+typedef struct {
+    uint8_t       uuid[16];
+    data_field_t  fields[DATA_FILED_MAX];
+    uint8_t       enabled;
+    uint8_t       ready;
+    device_data_t data;
+} gateway_device_t;
+
+/* Private variable ======================================== */
+static gateway_device_t devices[APP_GATEWAY_DEVICE_MAX];
+
+/* Private function ======================================== */
+static void gateway_task_handle(void* param);
 
 void gateway_init(void) {
-
+    // TODO create task and wait for authentication finish.
 }
 
 void gateway_device_data_set(beacon_data_t* data) {
+    // Check the uuid.
+    for(uint8_t i = 0;i < APP_GATEWAY_DEVICE_MAX; i++) {
+        gateway_device_t *dev = &devices[i];
 
+        // TODO Use lock to safe share resource.
+
+        // Device are enabled to received data on the gateway
+        if(dev->enabled == 0) {
+            continue;
+        }
+
+        if(memcmp(data->uuid, dev->uuid, 16)) {
+            continue;
+        }
+
+        // Get device IP
+        dev->data.addr[0] = data->addr[0];
+        dev->data.addr[1] = data->addr[1];
+        dev->data.addr[2] = data->addr[2];
+        dev->data.addr[3] = data->addr[3];
+        dev->data.addr[4] = data->addr[4];
+        dev->data.addr[5] = data->addr[5];
+
+        // Get data
+        dev->data.data[0] = data->udata[0];
+        dev->data.data[1] = data->udata[1];
+        dev->data.data[2] = data->udata[2];
+        dev->data.data[3] = data->udata[3];
+
+        dev->data.prop    = data->rssi;
+        ddv->ready        = 1;
+
+        // TODO notify new data ready to send
+        break;
+    }
+}
+
+/**********************************************************************
+ * Implement private function.
+ *********************************************************************/
+static void gateway_task_handle(void* param) {
+    (void)param;
+
+    while(true) {
+        for(uint8_t i  = 0; i < APP_GATEWAY_DEVICE_MAX; i++) {
+            gateway_device_t *dev = &devices[i];
+            if(dev->ready) {
+                dev->ready = 0;
+
+                // Copy device data
+                device_data_t data = dev->data;
+
+                // TODO Convert data to json message
+
+
+
+                //TODO handle and send data to MQTT
+            }
+        }
+    }
 }
